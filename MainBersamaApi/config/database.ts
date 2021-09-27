68d9821e279eb828ac7f8a1ce39cac1b10466891
cocow15 <79132450+dimasWisnu420@users.noreply.github.com>
2021-09-27 09:45:06 +0700

    Ready

--- MainBersamaApi/config/database.ts
+++ MainBersamaApi/config/database.ts
@@ -34,21 +34,21 @@ const databaseConfig: DatabaseConfig = {
     | npm i mysql
     |
     */
-    mysql: {
-      client: 'mysql',
-      connection: {
-        host: Env.get('MYSQL_HOST'),
-        port: Env.get('MYSQL_PORT'),
-        user: Env.get('MYSQL_USER'),
-        password: Env.get('MYSQL_PASSWORD', ''),
-        database: Env.get('MYSQL_DB_NAME'),
-      },
-      migrations: {
-        naturalSort: true,
-      },
-      healthCheck: false,
-      debug: false,
-    },
+    // mysql: {
+    //   client: 'mysql',
+    //   connection: {
+    //     host: Env.get('MYSQL_HOST'),
+    //     port: Env.get('MYSQL_PORT'),
+    //     user: Env.get('MYSQL_USER'),
+    //     password: Env.get('MYSQL_PASSWORD', ''),
+    //     database: Env.get('MYSQL_DB_NAME'),
+    //   },
+    //   migrations: {
+    //     naturalSort: true,
+    //   },
+    //   healthCheck: false,
+    //   debug: false,
+    // },
     pg: {
       client: 'pg',
       connection: {
